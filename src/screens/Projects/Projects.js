e3fe8da0972bb63e38187544f0f25bebc03c147c
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-16 16:18:02 -0400

    init section for 12 step alphabet app

--- src/screens/Projects/Projects.js
+++ src/screens/Projects/Projects.js
@@ -3,14 +3,37 @@ import { createUseStyles } from 'react-jss'
 const useStyles = createUseStyles({
   projectsContainer: {
   display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'flex-start',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
   zIndex: -1,
   objectFit: 'cover',
   width: '100w',
-  pointerEvents: 'none'
-},
+    pointerEvents: 'none',
+    backgroundColor: 'blue',
+    color: 'white',
+    wdith: '100vw',
+    height: '100vh'
+  },
+  
+  
+  project: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderBottom: '5px solid black',
+    borderTop: '5px solid black',
+  },
+
+  projectInfo: {
+  
+  },
+  
+  video: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+  }
 })
 
 export default useStyles;
--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import useStyles from './Projects.js'
-
+import TwelveStepAlphabet from '../../Assets/myc-portfolio_projects_12step.mp4'
 import Layout from '../../components/shared/Layout/Layout.jsx'
 
 export default function Projects() {
@@ -8,7 +8,41 @@ export default function Projects() {
   return (
     <>
       <Layout>
-        <div className={classes.projectsContainer}>Projects</div>
+        <div className={classes.projectsContainer}>
+          <div className={classes.project}>
+            <div className={classes.projectInfo}>
+              <h1>12 Step Alphabet</h1>
+              <p>
+                Fullstack React + Rails 12-step recovery application and meeting
+                guide for AA, CMA, NA, and ALANON
+              </p>
+              Features include:
+              <ul>
+                <li>
+                  Dataset of 100,000+ entries for 12 step meetings in the
+                  continental United States
+                </li>
+                <li>
+                  Mapbox APIs to render full color maps for each meeting entry
+                </li>
+                <li>
+                  Mapbox GeoLocation API for full geolocation functionality to
+                  retrieve user location
+                </li>
+              </ul>
+              <a
+                href="https://www.github.com"
+              >
+                VIEW THE CODE
+              </a>
+            </div>
+            <div className={classes.video}>
+              <video autoPlay muted loop pointerEvents="none" width="575px">
+                <source src={TwelveStepAlphabet} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   )
