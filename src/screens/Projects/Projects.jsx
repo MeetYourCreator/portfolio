85646255e1a2b47924cbb031b3b32a9d050fdfa4
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-18 11:27:40 -0400

    refactored Project.js to Project.css

--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -1,47 +1,31 @@
 import React from 'react'
-import useStyles from './Projects.js'
-import TwelveStepAlphabet from '../../Assets/myc-portfolio_projects_12step.mp4'
+import './Projects.css'
+
 import Layout from '../../components/shared/Layout/Layout.jsx'
+import Project from '../../components/Project/Project.jsx'
+
+import TwelveStepAlphabet from "../../Assets/myc-portfolio_projects_12step.mp4"
 
 export default function Projects() {
-  const classes = useStyles()
+  
   return (
     <>
       <Layout>
-        <div className={classes.projectsContainer}>
-          <div className={classes.project}>
-            <div className={classes.projectInfo}>
-              <h1>12 Step Alphabet</h1>
-              <p>
-                Fullstack React + Rails 12-step recovery application and meeting
-                guide for AA, CMA, NA, and ALANON
-              </p>
-              Features include:
-              <ul>
-                <li>
-                  Dataset of 100,000+ entries for 12 step meetings in the
-                  continental United States
-                </li>
-                <li>
-                  Mapbox APIs to render full color maps for each meeting entry
-                </li>
-                <li>
-                  Mapbox GeoLocation API for full geolocation functionality to
-                  retrieve user location
-                </li>
-              </ul>
-              <a
-                href="https://www.github.com"
-              >
-                VIEW THE CODE
-              </a>
-            </div>
-            <div className={classes.video}>
-              <video autoPlay muted loop pointerEvents="none" width="575px">
-                <source src={TwelveStepAlphabet} type="video/mp4" />
-              </video>
-            </div>
-          </div>
+        <div className="projects-container">
+          <Project
+            src={TwelveStepAlphabet}
+            title="12 Step Alphabet"
+            description="Fullstack React + Rails 12 Step Recovery Application"
+            descriptionFeature1="Dataset of 100,000+ entries for 12 step meetings in the
+              continental United States"
+            descriptionFeature2="Mapbox APIs to render full color maps for each meeting entry"
+            descriptionFeature3="Mapbox GeoLocation API for full geolocation functionality to retrieve user location"
+            descriptionFeature4="Comprehensive meeting directory for Alcoholics Anonymous, Crystal Meth Anonymous, Narcotics Anonymous, and ALANON"
+            codeSRC="https://github.com/crimethinkWithMe/twelve-step-alphabet/tree/develop"
+            projectSRC=""
+          />
+          <Project />
+          <Project />
         </div>
       </Layout>
     </>
