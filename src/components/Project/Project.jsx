307dcae55cf2690e052115f51347bc1e53a8300b
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-19 16:24:59 -0400

    styling for external links

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -21,14 +21,14 @@ const Project = ({
           <h1 className="project-title">{title}</h1>
           <p className="project-description">{description}</p>
           <p className="project-description-byline">{descriptionByline}</p>
-          <span className="features-heading">Application features:</span>
+          <span className="features-heading">Project highlights:</span>
           <ul>
             <li>{descriptionFeature1}</li>
             <li>{descriptionFeature2}</li>
             <li>{descriptionFeature3}</li>
             <li>{descriptionFeature4}</li>
           </ul>
-          <div classNAme="external-links">
+          <div className="external-links">
             <a href={codeSRC}>View Code</a> ||
             <a href={projectSRC}>View Project</a>
           </div>
