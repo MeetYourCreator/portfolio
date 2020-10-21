3fd75d284cf9dc4b25e1059acba09d9c5c3bcdcb
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-21 09:18:00 -0400

    added whileHover animation effects to li elements in Nav bar

--- src/components/shared/Nav/Nav.jsx
+++ src/components/shared/Nav/Nav.jsx
@@ -1,33 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import useStyles from './Nav.js'
 
 export default function Nav() {
   const classes = useStyles()
   
   return (
-   
-      <ul className={classes.navContainer}>
-        <li>
-          <Link to="/" className={classes.navLink}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/projects" className={classes.navLink}>
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className={classes.navLink}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className={classes.navLink}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <ul className={classes.navContainer}>
+      <motion.li
+        whileHover={{
+          scale: 1.2,
+          textShadow: "0px 0px 8px rgb(73,162,190)",
+          type: "tween",
+        }}
+      >
+        <Link to="/" className={classes.navLink}>
+          Home
+        </Link>
+      </motion.li>
+      <motion.li
+        whileHover={{
+          scale: 1.2,
+          textShadow: "0px 0px 8px rgb(73,162,190",
+        }}
+      >
+        <Link to="/projects" className={classes.navLink}>
+          Projects
+        </Link>
+      </motion.li>
+      <motion.li
+        whileHover={{
+          scale: 1.2,
+          textShadow: "0px 0px 8px rgb(73,162,190",
+        }}
+      >
+        <Link to="/about" className={classes.navLink}>
+          About
+        </Link>
+      </motion.li>
+      <motion.li
+        whileHover={{
+          scale: 1.2,
+          textShadow: "0px 0px 8px rgb(73,162,190",
+        }}
+      >
+        <Link to="/contact" className={classes.navLink}>
+          Contact
+        </Link>
+      </motion.li>
+    </ul>
   )
 }
