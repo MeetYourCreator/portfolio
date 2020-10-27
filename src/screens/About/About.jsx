d313dc665e9efa685b705093a9bad0a72f4bb4c2
ramizackaryshamir <ramizackshamir@icloud.com>
2020-10-27 16:52:12 -0400

    alt props for img elements

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -21,29 +21,53 @@ export default function About() {
             <h2 className={classes.aboutTitleSection}>Press</h2>
             <div className={classes.pressBox}>
               <a href="https://www.printmag.com/post/the-spirit-of-barney-rosset-lives-on-the-train-to-pokipse">
-                <img src={Printmag} className={classes.image} />
+                <img
+                  src={Printmag}
+                  className={classes.image}
+                  alt="Print Magazine article layout"
+                />
               </a>
             </div>
             <div className={classes.pressBox}>
               <a href="https://www.huffpost.com/entry/rami-shamir-interview_b_3710947">
-                <img src={HuffingtonPost} className={classes.image} />
+                <img
+                  src={HuffingtonPost}
+                  className={classes.image}
+                  alt="Huffington Post interview layout"
+                />
               </a>
             </div>
             <div className={classes.pressBox}>
               <a href="https://www.wsj.com/articles/BL-METROB-14093">
-                <img src={WSJ} className={classes.image} />
+                <img
+                  src={WSJ}
+                  className={classes.image}
+                  alt="Wall Street Journal article layout"
+                />
               </a>
             </div>
             <div className={classes.pressBox}>
               <a href="https://www.breitbart.com/politics/2011/10/24/fl-bankers-wife-abandons-children-for-very-important-occupywallstreet-cleaning-duties/">
-                <img src={Breitbart} className={classes.image} />
+                <img
+                  src={Breitbart}
+                  className={classes.image}
+                  alt="Breitbart Magazine article layout"
+                />
               </a>
             </div>
             <div className={classes.pressBox}>
-              <img src={Adbusters103} className={classes.image} />
+              <img
+                src={Adbusters103}
+                className={classes.image}
+                alt="Adbusters Magazine article layout"
+              />
             </div>
             <div className={classes.pressBox}>
-              <img src={TranslatingAnarchy} className={classes.image} />
+              <img
+                src={TranslatingAnarchy}
+                className={classes.image}
+                alt="Translating Anarchy book layout"
+              />
             </div>
           </div>
         </div>
