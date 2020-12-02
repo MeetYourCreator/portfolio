cc36d0feaeb9c813266790dc134e31011b4926eb
ramizackaryshamir <ramizackshamir@icloud.com>
2020-12-02 08:41:42 -0500

    Styling @media, scolling disabled for all viewport sizes

--- src/components/shared/Layout/Layout.js
+++ src/components/shared/Layout/Layout.js
@@ -4,26 +4,25 @@ const useStyles = createUseStyles({
   layout: {
     display: 'flex',
     flexDirection: 'column',
-    // width: '100vw',
+    width: '100vw',
+    height: '100vh',
     backgroundColor: 'rgb(0,0,0)',
-    overflow: 'hidden'
+   
   },
 
   layoutChildren: {
-    overflow: 'hidden',
-    scrollBehaviour: 'smooth',
-    scrollSnapType: 'y mandatory',
-    scrollSnapAlign: 'center'
+    overflowX: 'hidden',
+    overflowY: 'auto',
   },
 
-  '@media (max-width: 415px)': {
-    layout: {
-      display: 'flex',
-      width: '100%',
-      padding: 0,
-      marginTop: 0,
-    }
-  }
+  // '@media (max-width: 415px)': {
+  //   layout: {
+  //     display: 'flex',
+  //     width: '100%',
+  //     padding: 0,
+  //     marginTop: 0,
+  //   }
+  // }
 
 })
 
