45ca17efa7df061683089089aa47fcc1e15f8449
ramizackaryshamir <ramizackshamir@icloud.com>
2020-12-02 11:50:01 -0500

    changed property for build key in package.json

---
+++ build/precache-manifest.531ffa840e2e6f967e29f9dcf821087d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6502aee2ac6458846d425a7aa3933140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b37a2bc505521345d49",
+    "url": "/static/css/main.d3c38727.chunk.css"
+  },
+  {
+    "revision": "275c3fc66d7f1596c16d",
+    "url": "/static/js/2.b4f19ccf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.b4f19ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b37a2bc505521345d49",
+    "url": "/static/js/main.d3d6f07b.chunk.js"
+  },
+  {
+    "revision": "1619c643ddc33d92895c",
+    "url": "/static/js/runtime-main.5bd80a1b.js"
+  },
+  {
+    "revision": "e0d7b282128e7365b26857f1c7dd3298",
+    "url": "/static/media/NEW MASTER FOR RENDER.e0d7b282.mp4"
+  },
+  {
+    "revision": "7a6456e6a11aa8fad4b655101f984051",
+    "url": "/static/media/myc_rami-zackary-shamir_portfolio_video-bckgrd.7a6456e6.mp4"
+  },
+  {
+    "revision": "f1b148e86fed105a73613104b32522a8",
+    "url": "/static/media/new-alternatives-vid.f1b148e8.mp4"
+  },
+  {
+    "revision": "d665bd353693d0dc80e874f9f04495a5",
+    "url": "/static/media/press_adbusters_103.d665bd35.png"
+  },
+  {
+    "revision": "5d2ccf6232ad8851f0fc38c5c51e76b6",
+    "url": "/static/media/press_breitbart.5d2ccf62.png"
+  },
+  {
+    "revision": "f3d3e67e997aa21d22f5c6823d811578",
+    "url": "/static/media/press_huffingtonpost.f3d3e67e.png"
+  },
+  {
+    "revision": "9a023ddc1170036b519adf4a5e43aae4",
+    "url": "/static/media/press_printmag.9a023ddc.png"
+  },
+  {
+    "revision": "11130ae0ec11504b16e62c0bf55f2128",
+    "url": "/static/media/press_translating-anarchy.11130ae0.png"
+  },
+  {
+    "revision": "8713d0707ebaac57130e62e8c646bf9b",
+    "url": "/static/media/press_wsj.8713d070.png"
+  }
+]);
