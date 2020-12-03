9c2d06ce20d01b94d8aa8c62ddec4cdc864e3054
ramizackaryshamir <ramizackshamir@icloud.com>
2020-12-03 13:31:52 -0500

    contact screen

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,12 +1,28 @@
 import React from "react"
 import "./Contact.css"
-
+import LinkedIn from "../../Assets/LinkedIn-icon.png"
+import GitHub from "../../Assets/github-logo.png"
 import Layout from "../../components/shared/Layout/Layout.jsx"
 
 const Contact = () => {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <section className="about-container">
+          <div className="info">
+            <h1 className="name">Rami Zackary Shamir</h1>
+            <h2 className="email">ramizackshamir@icloud.com</h2>
+          </div>
+          <aside className="social">
+            <a href="https://www.linkedin.com/in/ramizackaryshamir/">
+              <img className="linkedin" src={LinkedIn} />
+            </a>
+            <a href="https://github.com/MeetYourCreator">
+              <img className="github" src={GitHub} />
+            </a>
+          </aside>
+        </section>
+      </Layout>
     </>
   )
 }
