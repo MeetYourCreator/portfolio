b4efda4a990a7cb88929605d3c16252f5679bbd7
MeetYourCreator <ramizackshamir@icloud.com>
2021-01-08 08:06:32 -0500

    fix copy on Prjects page

--- src/screens/Projects/Projects.jsx
+++ src/screens/Projects/Projects.jsx
@@ -15,7 +15,7 @@ export default function Projects() {
             src={TwelveStepAlphabet}
             title="12 Step Alphabet"
             description="Fullstack React on Rails"
-            descriptionByline="Twelve step recovery application"
+            descriptionByline="12 Step Recovery Application"
             descriptionFeature1="Dataset of 100,000+ entries to create a aomprehensive meeting directory for Alcoholics Anonymous, Crystal Meth Anonymous, Narcotics Anonymous, and ALANON."
             descriptionFeature2="Mapbox APIs to render full color maps for each meeting entry, Mapbox GeoLocation API to retrieve user location, and  Mapbox Directions API to provide full navigation functionality."
             descriptionFeature3="Full CRUD functionality for users to post, edit, and delete reviews of their favorite (and not-so-favorite) 12 step meetings."
@@ -29,13 +29,14 @@ export default function Projects() {
             descriptionByline="Revamp of website for NYC non-profit"
             descriptionFeature1="Worked with a design team to translate wire-frames into a scalable high fidelity, front-end architecture."
             descriptionFeature2="Optimized the delivery of code assets to browsers across a wide spectrum of devices."
-Auster            codeSRC="https://bit.ly/newAlt_gh"
+            descriptionFeature3="Seemless integratation of MongoDB, Express, React, and Node.js to implement full CRUD functionality."
+            codeSRC="https://bit.ly/newAlt_gh"
             projectSRC="https://bit.ly/newAlt_site"
           />
           <Project
             src={NYTimes}
             title="NYT News Widget"
-            description="Client-side Vanilla Javascript News Widget"
+            description="Vanilla JavaScript News Widget"
             descriptionByline="Up-to-the-minute stories from the New York Times"
             descriptionFeature1="No libraries. No modules. No frameworks. 100% vanilla JavaScript/HTML/CSS NYT news widget."
             descriptionFeature2="Utilizes New York Times 'Times Wire API' to retrieve up-to-the-minute stream of nytimes articles."
