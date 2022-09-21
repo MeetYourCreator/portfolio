83e93843a984d92aab64271c0c8601da6f895c38
MeetYourCreator <rzshamir@icloud.com>
2022-09-20 23:08:14 -0400

    init reworked Messaging screen

--- src/screens/Messaging/Messaging.jsx
+++ src/screens/Messaging/Messaging.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
-
+import {Adbusters, WSJ, PM, TA, HP, BB} from './style.js';
 import GridLayout from 'react-grid-layout';
 
 class Messaging extends React.Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, minW: 2, maxW: 4},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2},
+      {i: 'a', x: 0, y: 5, w: 5, h: 2},
+      {i: 'b', x: 0, y: 3, w: 2, h: 10},
+      {i: 'c', x: 2, y: 2, w: 3, h: 10},
+      {i: 'd', x: 3, y: 5, w: 3, h: 10},
+      {i: 'e', x: 4, y: 9, w: 3, h: 10},
+      {i: 'f', x: 5, y: 9, w: 3, h: 10},
     ];
     return (
-      <GridLayout
-        className='layout'
-        layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={1200}
-      >
-        <div key='a'>Kickstarters</div>
-        <div key='b'>Occupy Wall Street</div>
-        <div key='c'>Graffiti Advertising</div>
-      </GridLayout>
+      <>
+        <section className='mssg-container'>
+          <GridLayout
+            className='layout'
+            layout={layout}
+            cols={10}
+            rowHeight={30}
+            width={1800}
+            autoSize={true}
+            margin={[0, 0]}
+            isDraggable={true}
+            isResizable={true}
+            compactType={'vertical'}
+            resizeHandles={['se']}
+          >
+            <Adbusters key='a' />
+            <WSJ key='b' />
+            <PM key='c' />
+            <TA key='d' />
+            <HP key='e' />
+            <BB key='f' />
+          </GridLayout>
+        </section>
+      </>
     );
   }
 }
---
+++ src/screens/Messaging/style.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import Adbusters103 from '../../Assets/press_adbusters_103.png';
+import WallStreetJournal from '../../Assets/press_wsj.png';
+import PrintMag from '../../Assets/press_printmag.png';
+import TranslatingAnarchy from '../../Assets/press_translating-anarchy.png';
+import Breitbart from '../../Assets/press_breitbart.png';
+import HuffingtonPost from '../../Assets/press_huffingtonpost.png';
+
+export const Adbusters = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${Adbusters103})
+`;
+
+export const WSJ = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${WallStreetJournal})
+`;
+
+export const PM = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${PrintMag})
+`;
+
+export const TA = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${TranslatingAnarchy})
+`;
+
+export const HP = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${HuffingtonPost})
+`;
+
+export const BB = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  background-image: url(${Breitbart})
+`;
