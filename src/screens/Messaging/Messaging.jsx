4be962ad0c853660050f8cea012365639fe388fe
MeetYourCreator <rzshamir@icloud.com>
2022-09-26 21:39:46 -0400

    Messaging page layout

--- src/screens/Messaging/Messaging.jsx
+++ src/screens/Messaging/Messaging.jsx
@@ -1,42 +1,45 @@
 import React from 'react';
-import {Adbusters, WSJ, PM, TA, HP, BB} from './style.js';
 import GridLayout from 'react-grid-layout';
+import Layout from '../../components/shared/Layout/Layout.jsx';
+import {MssgContainer, Adbusters, WSJ, PM, TA, HP, BB} from './style.js';
 
 class Messaging extends React.Component {
   render() {
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
-      {i: 'a', x: 0, y: 5, w: 5, h: 2},
-      {i: 'b', x: 0, y: 3, w: 2, h: 10},
-      {i: 'c', x: 2, y: 2, w: 3, h: 10},
-      {i: 'd', x: 3, y: 5, w: 3, h: 10},
-      {i: 'e', x: 4, y: 9, w: 3, h: 10},
-      {i: 'f', x: 5, y: 9, w: 3, h: 10},
+      {i: 'adbusters', x: 0, y: 9, w: 10, h: 2},
+      {i: 'wsj', x: 0, y: 3, w: 2, h: 13},
+      {i: 'pm', x: 2, y: 2, w: 3, h: 6},
+      {i: 'ta', x: 2, y: 6, w: 3, h: 4},
+      {i: 'hp', x: 2, y: 7, w: 8, h: 3},
+      {i: 'bb', x: 5, y: 0, w: 5, h: 10},
     ];
     return (
       <>
-        <section className='mssg-container'>
-          <GridLayout
-            className='layout'
-            layout={layout}
-            cols={10}
-            rowHeight={30}
-            width={1800}
-            autoSize={true}
-            margin={[0, 0]}
-            isDraggable={true}
-            isResizable={true}
-            compactType={'vertical'}
-            resizeHandles={['se']}
-          >
-            <Adbusters key='a' />
-            <WSJ key='b' />
-            <PM key='c' />
-            <TA key='d' />
-            <HP key='e' />
-            <BB key='f' />
-          </GridLayout>
-        </section>
+        <Layout>
+          <MssgContainer>
+            <GridLayout
+              className='layout'
+              layout={layout}
+              cols={10}
+              rowHeight={30}
+              width={1800}
+              autoSize={true}
+              margin={[0, 0]}
+              isDraggable={true}
+              isResizable={true}
+              compactType={'vertical'}
+              resizeHandles={['se']}
+            >
+              <Adbusters className='adbusters' key='adbusters' />
+              <WSJ key='wsj' />
+              <PM key='pm' />
+              <TA key='ta' />
+              <HP key='hp' />
+              <BB key='bb' />
+            </GridLayout>
+          </MssgContainer>
+        </Layout>
       </>
     );
   }
--- src/screens/Messaging/style.js
+++ src/screens/Messaging/style.js
@@ -6,38 +6,40 @@ import TranslatingAnarchy from '../../Assets/press_translating-anarchy.png';
 import Breitbart from '../../Assets/press_breitbart.png';
 import HuffingtonPost from '../../Assets/press_huffingtonpost.png';
 
+export const MssgContainer = styled.section`
+display: flex;
+`
 export const Adbusters = styled.section`
-  padding: 4em;
   background: papayawhip;
-  background-image: url(${Adbusters103})
+  background-position: center;
+  background-image: url(${Adbusters103});
 `;
 
 export const WSJ = styled.section`
-  padding: 4em;
   background: papayawhip;
-  background-image: url(${WallStreetJournal})
+  background-position: center top;
+  background-image: url(${WallStreetJournal});
+  box-sizing:border-box;
 `;
 
 export const PM = styled.section`
-  padding: 4em;
   background: papayawhip;
+  background-position: left top;
   background-image: url(${PrintMag})
 `;
 
 export const TA = styled.section`
-  padding: 4em;
   background: papayawhip;
   background-image: url(${TranslatingAnarchy})
 `;
 
 export const HP = styled.section`
-  padding: 4em;
   background: papayawhip;
   background-image: url(${HuffingtonPost})
 `;
 
 export const BB = styled.section`
-  padding: 4em;
   background: papayawhip;
+  background-position: center;
   background-image: url(${Breitbart})
 `;
