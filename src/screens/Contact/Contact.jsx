b603777e0ba44ca2f7fd1df5dd63818aa84d0e8a
MeetYourCreator <rzshamir@icloud.com>
2022-12-02 22:19:58 -0500

    correct email address

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import "./Contact.css"
-import LinkedIn from "../../Assets/LinkedIn-icon.png"
-import GitHub from "../../Assets/github-logo.png"
-import Layout from "../../components/shared/Layout/Layout.jsx"
+import React from "react";
+import "./Contact.css";
+import LinkedIn from "../../Assets/LinkedIn-icon.png";
+import GitHub from "../../Assets/github-logo.png";
+import Layout from "../../components/shared/Layout/Layout.jsx";
 
 const Contact = () => {
   return (
@@ -11,20 +11,20 @@ const Contact = () => {
         <section className="about-container">
           <div className="info">
             <h1 className="name">Rami Zackary Shamir</h1>
-            <h2 className="email">ramizackshamir@icloud.com</h2>
+            <h2 className="email">rzshamir@icloud.com</h2>
           </div>
           <aside className="social">
             <a href="https://www.linkedin.com/in/ramizackaryshamir/">
-              <img className="linkedin" src={LinkedIn} alt='LinkedIn Logo'/>
+              <img className="linkedin" src={LinkedIn} alt="LinkedIn Logo" />
             </a>
             <a href="https://github.com/MeetYourCreator">
-              <img className="github" src={GitHub} alt='GitHub Logo'/>
+              <img className="github" src={GitHub} alt="GitHub Logo" />
             </a>
           </aside>
         </section>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
